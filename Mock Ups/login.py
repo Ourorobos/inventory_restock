d471aacef1bcf07378b8793f5abdbc276a34699b
Ourorobos <Ourorobos.digums@gmail.com>
2023-11-18 14:53:02 -0700

    Finished the login mock up

--- Mock Ups/login.py
+++ Mock Ups/login.py
@@ -2,17 +2,40 @@
 
 #imports
 import tkinter
+from tkinter import ttk
 
 #global vars, lists, ect
-memory_users = [["admin","tkinter","admin"], #this will be found on the server
+memory_users = [["admin","tkinter","admin"], #this will be found on the server but in the form of user obj/class
                 ["shipping","ship","editor"]]
 
+login_root = tkinter.Tk()
+name_label = ttk.Label(login_root,text = "User Name")
+password_label = ttk.Label(login_root,text = "Password")
+name_entry = ttk.Entry(login_root)
+password_entry = ttk.Entry(login_root)
+submit_button = ttk.Button(login_root,text = "Login")
+
+#Funcitons
+def login_func():
+    user_name = name_entry.get()
+    user_password = password_entry.get()
+    for user in memory_users:
+        if user[0] == user_name and user[1] == user_password:
+            print("Wellcome")
+
 #classes
-class user:
 
-    def __init__(self,name,password,type):
-        self.name = name
-        self.password = password
-        self.type = type
+#Set up block
+login_root.title("Login")
+login_root.geometry("300x150")
+submit_button.config(command = login_func)
 
-# !!!! main code block !!!!
+name_label.pack()
+name_entry.pack()
+password_label.pack()
+password_entry.pack()
+submit_button.pack()
+
+
+#Run block
+login_root.mainloop()
